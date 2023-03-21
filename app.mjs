47323b9a8a6f2b8da5f8570a2c7469b1268a8844
xuxihai <xuxihai@csii.com.cn>
2023-03-21 14:50:20 +0800

    feat: maxAge update

--- app.mjs
+++ app.mjs
@@ -1,12 +1,11 @@
-
-import express from 'express';
-import logger from 'morgan';
-import compression from 'compression';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
-import sse from './sse.mjs';
-import chatgptRouter from './chatgpt.mjs';
+import express from "express";
+import logger from "morgan";
+import compression from "compression";
+import path from "path";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import sse from "./sse.mjs";
+import chatgptRouter from "./chatgpt.mjs";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -17,8 +16,8 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-app.use(sse())
+app.use(express.static(path.join(__dirname, "public"), { maxAge: 3600 * 30 }));
+app.use(sse());
 
 app.use("/backend-api", chatgptRouter);
 

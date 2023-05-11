eba6986d2d82a43ff0f5b30400429e224c744a7e
xuxihai <xuxihai@csii.com.cn>
2023-05-11 15:49:12 +0800

    dynamic check user/pass

--- auth.mjs
+++ auth.mjs
@@ -1,17 +1,15 @@
 import fs from "fs";
 
-let passObj = {};
-
-try {
-  const allFileContents = fs.readFileSync("./passwd.txt", "utf-8");
-  allFileContents.split(/\r?\n/).forEach((line) => {
-    const parts = line.split(":");
-    passObj[parts[0]] = parts[1];
-  });
-} catch (err) {
-  console.log(err);
-}
-
 export default function(user, pass) {
-  return passObj[user] && passObj[user] === pass;
+  try {
+    let passObj = {};
+    const allFileContents = fs.readFileSync("./passwd.txt", "utf-8");
+    allFileContents.split(/\r?\n/).forEach((line) => {
+      const parts = line.split(":");
+      passObj[parts[0]] = parts[1];
+    });
+    return passObj[user] && passObj[user] === pass;
+  } catch (err) {
+    console.log(err);
+  }
 }
